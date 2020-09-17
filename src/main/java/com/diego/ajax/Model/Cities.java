a1d82fb657ef8d0cd07cbdc71e1f5c1c08229213
DiegoGPereira <diiegopereiraa_@hotmail.com>
2020-09-17 01:54:23 -0300

    Upload files initial

---
+++ src/main/java/com/diego/ajax/Model/Cities.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.diego.ajax.Model;
+
+/**
+ *
+ * @author Diego
+ */
+public class Cities {
+
+    private String name;
+    private String country;
+
+    public Cities(String name, String country) {
+        this.name = name;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+}
